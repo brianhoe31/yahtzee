87aa87f07bcf6aba1c88ccd804720f71fdf3761f
Brian <brianhoe31@gmail.com>
2021-04-22 10:44:26 -0700

    Add method to prevent more than 3 rolls & disallow re-using a used score line

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import './RuleRow.css'
 
 class RuleRow extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      clicked: false
+    }
+    this.handleClick = this.handleClick.bind(this); 
+  }
+  handleClick() {
+    this.props.doScore();
+
+    this.setState({
+      clicked: true
+    })
+  }
+
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
+      <tr className={this.state.clicked ? "RuleRow RuleRow-disabled" : "RuleRow RuleRow-active"}  onClick={this.handleClick}>
+        <td className="RuleRow-name" disabled={this.state.clicked}>{this.props.name}</td>
+        <td className="RuleRow-score" disabled={this.state.clicked}>{this.props.score}</td>
       </tr>
     )
   }
